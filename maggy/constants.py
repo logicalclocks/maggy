d6dedfbf2e79591f5cb8260a445ca7a635e07656
Moritz Meister <meister.mo@gmail.com>
2019-05-19 12:47:03 +0200

    Fix typo in docstring

--- maggy/constants.py
+++ maggy/constants.py
@@ -4,6 +4,6 @@ Constants used in Maggy: Allowed datatypes etc.
 import numpy as np
 
 class USER_FCT:
-    """User traingin function specifics.
+    """User training function specifics.
     """
     RETURN_TYPES = (float, int, np.number)
