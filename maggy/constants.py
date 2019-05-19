49dfb5f8caa058c3a9092ed00b1086fcdd98ef1b
Moritz Meister <meister.mo@gmail.com>
2019-05-19 12:38:22 +0200

    Change Docstring

--- maggy/constants.py
+++ maggy/constants.py
@@ -4,6 +4,6 @@ Constants used in Maggy: Allowed datatypes etc.
 import numpy as np
 
 class USER_FCT:
-    """Return datatypes allowed for user defined training function.
+    """User traingin function specifics.
     """
     RETURN_TYPES = (float, int, np.number)
