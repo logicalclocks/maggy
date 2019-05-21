dbe99f482884a04b4e065af0feac9c9fda604083
Moritz Meister <meister.mo@gmail.com>
2019-05-21 14:30:00 +0200

    Add tf as extra_require

--- setup.py
+++ setup.py
@@ -10,6 +10,12 @@ def read(fname):
 setup(
     name='maggy',
     version=__version__,
+    install_requires=[
+        'numpy'
+    ],
+    extras_require={
+        'tf': ['tensorflow']
+    },
     author='Moritz Meister',
     author_email='meister.mo@gmail.com',
     description='',
