ecf5ceb19bfde57fdb7035df72adcd45e14e9f7b
Moritz Meister <meister.mo@gmail.com>
2019-05-27 16:02:26 +0200

    Test stupid median rule

--- maggy/earlystop/medianrule.py
+++ maggy/earlystop/medianrule.py
@@ -12,6 +12,13 @@ class MedianStoppingRule(AbstractEarlyStop):
 
         stop = []
 
+        try:
+            results = []
+            median = statistics.median(results)
+        except Exception as e:
+            print(e)
+            raise
+
         for trial_id, trial in to_check.items():
 
             results = []
