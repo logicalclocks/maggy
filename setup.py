337c3862a16db4ded21fbbab3484c9ba10ce5ff8
Moritz Meister <meister.mo@gmail.com>
2019-10-14 16:50:34 +0200

    Add dependencies

--- setup.py
+++ setup.py
@@ -11,15 +11,31 @@ setup(
     name='maggy',
     version=__version__,
     install_requires=[
-        'numpy'
+        'numpy==1.16.5'
     ],
+    extras_require={
+        'pydoop': ['pydoop'],
+        'tf': ['tensorflow==1.14.0'],
+        'docs': [
+            'sphinx==1.8.5',
+            'sphinx-autobuild',
+            'recommonmark',
+            'sphinx_rtd_theme',
+            'jupyter_sphinx_theme'
+        ],
+        'test': [
+            'pylint',
+            'pytest',
+        ],
+        'spark': ['pyspark==2.4.3']
+    },
     author='Moritz Meister',
     author_email='meister.mo@gmail.com',
     description='',
     license='GNU Affero General Public License v3',
     keywords='Hyperparameter, Optimization, Auto-ML, Hops, Hadoop, TensorFlow, Spark',
     url='https://github.com/logicalclocks/maggy',
-    # download_url = 'http://snurran.sics.se/hops/hops-util-py/hops-' + __version__ + '.tar.gz',
+    download_url='http://snurran.sics.se/hops/maggy/maggy-' + __version__ + '.tar.gz',
     packages=find_packages(),
     long_description=read('README.rst'),
     classifiers=[
