0ca54c5604231d3aa5942fe327ffa4bb124abaa4
moritzmeister <meister.mo@gmail.com>
2020-01-24 17:51:04 +0100

    Fix import bug

--- maggy/optimizer/__init__.py
+++ maggy/optimizer/__init__.py
@@ -3,6 +3,6 @@ from maggy.optimizer import abstractoptimizer, randomsearch, asha, singlerun
 AbstractOptimizer = abstractoptimizer.AbstractOptimizer
 RandomSearch = randomsearch.RandomSearch
 Asha = asha.Asha
-SingleRun = singlerun.singlerun
+SingleRun = singlerun.SingleRun
 
 __all__ = ["AbstractOptimizer", "RandomSearch", "Asha", "SingleRun"]
