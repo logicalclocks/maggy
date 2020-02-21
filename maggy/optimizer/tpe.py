6fb208ff41947d7cfcc2e5e4fa5771afcd0dd42b
Kai Jeggle <kai@logicalclocks.com>
2020-02-21 10:57:45 +0100

    added logs

--- maggy/optimizer/tpe.py
+++ maggy/optimizer/tpe.py
@@ -25,6 +25,7 @@ class TPE(AbstractOptimizer):
         self.log_file = "hdfs:///Projects/playground/Logs/tpe.log"
         if not hdfs.exists(self.log_file):
             hdfs.dump("", self.log_file)
+        self.fd = hdfs.open_file(self.log_file, flags="w")
         self._log("Initialized Logger")
 
         # keep track of the model (i.e the kernel density estimators l & g)
@@ -58,68 +59,72 @@ class TPE(AbstractOptimizer):
     def get_suggestion(self, trial=None):
         """Returns Trial instantiated with hparams that maximize the Expected Improvement"""
 
-        self._log("Get Suggestion")
+        try:
+            self._log("Get Suggestion")
 
-        # first sample randomly to warmup
-        # maybe it would be better to check the if clause with existance of model or trial
-        if self.random_warmup_trials:
-            return self.random_warmup_trials.pop()
+            # first sample randomly to warmup
+            # maybe it would be better to check the if clause with existance of model or trial
+            if self.random_warmup_trials:
+                return self.random_warmup_trials.pop()
 
-        self._log("Start updateing model")
+            self._log("Start updateing model")
 
-        self._update_model()
+            self._update_model()
 
-        self._log("Model {}".format(str(self.model)))
+            self._log("Model {}".format(str(self.model)))
 
-        if not self.model or np.random.rand() < self.random_fraction:
-            return Trial(self.searchspace.get_random_parameter_values(1)[0])
+            if not self.model or np.random.rand() < self.random_fraction:
+                return Trial(self.searchspace.get_random_parameter_values(1)[0])
 
-        best = -np.inf
-        best_sample = None
+            best = -np.inf
+            best_sample = None
 
-        kde_good = self.model["good"]
-        kde_bad = self.model["bad"]
+            kde_good = self.model["good"]
+            kde_bad = self.model["bad"]
 
-        # loop through potential samples
-        for sample in range(self.num_samples):
-            idx = np.random.randint(0, len(kde_good.data))
-            obs = kde_good.data[idx]
-            # todo make definition of bounds in search space more explicit, below is interim solution
-            bounds = np.array(
-                [spec[1] for hp, spec in self.searchspace.to_dict().items()]
-            ).T  # ndarray with shape (2, n_hparams)
-            sample_vector = []
+            # loop through potential samples
+            for sample in range(self.num_samples):
+                idx = np.random.randint(0, len(kde_good.data))
+                obs = kde_good.data[idx]
+                # todo make definition of bounds in search space more explicit, below is interim solution
+                bounds = np.array(
+                    [spec[1] for hp, spec in self.searchspace.to_dict().items()]
+                ).T  # ndarray with shape (2, n_hparams)
+                sample_vector = []
 
-            # loop through hparams
-            for mean, bw, low, high in zip(obs, kde_good.bw, bounds[0], bounds[1]):
-                # clip by min bw and multiply by factor to favor more exploration
-                bw = max(bw, self.min_bw) * self.bw_factor
+                # loop through hparams
+                for mean, bw, low, high in zip(obs, kde_good.bw, bounds[0], bounds[1]):
+                    # clip by min bw and multiply by factor to favor more exploration
+                    bw = max(bw, self.min_bw) * self.bw_factor
 
-                a, b = (
-                    (low - mean) / bw,
-                    (high - mean) / bw,
-                )  # see scipy documentation for more info
-                rv = sps.truncnorm(a, b, loc=mean, scale=bw)
-                sample_vector.append(rv)
+                    a, b = (
+                        (low - mean) / bw,
+                        (high - mean) / bw,
+                    )  # see scipy documentation for more info
+                    rv = sps.truncnorm(a, b, loc=mean, scale=bw)
+                    sample_vector.append(rv)
 
-            # calculate EI for current sample
-            ei_val = TPE._calculate_ei(sample_vector, kde_good, kde_bad)
+                # calculate EI for current sample
+                ei_val = TPE._calculate_ei(sample_vector, kde_good, kde_bad)
 
-            if ei_val > best:
-                best = ei_val
-                best_sample = sample_vector
+                if ei_val > best:
+                    best = ei_val
+                    best_sample = sample_vector
 
-        # transform sample representation to dict
-        # todo have general helper functions to change between representations
-        hparam_names = list(self.searchspace.names().keys())
-        best_sample_dict = {
-            hparam_name: hparam
-            for hparam_name, hparam in zip(hparam_names, best_sample)
-        }
+            # transform sample representation to dict
+            # todo have general helper functions to change between representations
+            hparam_names = list(self.searchspace.names().keys())
+            best_sample_dict = {
+                hparam_name: hparam
+                for hparam_name, hparam in zip(hparam_names, best_sample)
+            }
 
-        self._log("Best Sample {}".format(best_sample_dict))
+            self._log("Best Sample {}".format(best_sample_dict))
 
-        return Trial(best_sample_dict)
+            return Trial(best_sample_dict)
+        except BaseException:
+            self.fd.flush()
+            self.fd.close()
 
     def finalize_experiment(self, trials):
         return
@@ -223,7 +228,7 @@ class TPE(AbstractOptimizer):
         return max(1e-32, kde_good.pdf(x)) / max(kde_bad.pdf(x), 1e-32)
 
     def _log(self, msg):
-        self.fd = hdfs.open_file(self.log_file, flags="w")
+        # self.fd = hdfs.open_file(self.log_file, flags="w")
         self.fd.write((msg + "\n").encode())
-        self.fd.flush()
-        self.fd.close()
+        # self.fd.flush()
+        # self.fd.close()
