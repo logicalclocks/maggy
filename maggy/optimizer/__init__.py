e81d3e86055bd6e216eb8eedd1e96c35a2695c01
Kai Jeggle <kai@logicalclocks.com>
2020-05-05 21:50:27 +0200

    bugfix

--- maggy/optimizer/__init__.py
+++ maggy/optimizer/__init__.py
@@ -1,9 +1,4 @@
-from maggy.optimizer import (
-    abstractoptimizer,
-    randomsearch,
-    asha,
-    singlerun,
-)
+from maggy.optimizer import abstractoptimizer, randomsearch, asha, singlerun
 
 AbstractOptimizer = abstractoptimizer.AbstractOptimizer
 RandomSearch = randomsearch.RandomSearch
