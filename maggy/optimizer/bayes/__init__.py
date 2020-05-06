3aeb5981eb104ca5d03225f600b167c53918d267
Kai Jeggle <kai@logicalclocks.com>
2020-05-06 15:55:31 +0200

    test sth

--- maggy/optimizer/bayes/__init__.py
+++ maggy/optimizer/bayes/__init__.py
@@ -1,11 +1,11 @@
-# from maggy.optimizer.bayes import base, simple, tpe
-#
-# BaseAsyncBO = base.BaseAsyncBO
-# SimpleAsyncBO = simple.SimpleAsyncBO
-# TPE = tpe.TPE
-#
-# __all__ = [
-#     "TPE",
-#     "BaseAsyncBO",
-#     "SimpleAsyncBO",
-# ]
+from maggy.optimizer.bayes import base, simple, tpe
+
+BaseAsyncBO = base.BaseAsyncBO
+SimpleAsyncBO = simple.SimpleAsyncBO
+TPE = tpe.TPE
+
+__all__ = [
+    "TPE",
+    "BaseAsyncBO",
+    "SimpleAsyncBO",
+]
