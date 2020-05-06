edb12c297e9753a794e2925a41cf43ba7c9304e9
Kai Jeggle <kai@logicalclocks.com>
2020-05-06 16:45:25 +0200

    test sth

--- maggy/__init__.py
+++ maggy/__init__.py
@@ -1,4 +1,8 @@
 from maggy import searchspace
+from maggy import trial  # noqa: F401
+from maggy import core  # noqa: F401
+from maggy import util  # noqa: F401
+from maggy import experiment  # noqa: F401
 
 Searchspace = searchspace.Searchspace
 
