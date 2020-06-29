887692765ec992d37e89ac09db8a15d38864cbb5
Sina Sheikholeslami <ssheikholeslami@gmail.com>
2020-06-29 18:30:42 +0200

    dataset debug prints

--- maggy/ablation/ablator/loco.py
+++ maggy/ablation/ablator/loco.py
@@ -45,6 +45,8 @@ class LOCO(AbstractAblator):
         # for dataset generators provided by users
         if self.ablation_study.custom_dataset_generator is not None:
             print('ABLATION: in get_dataset_generator, setting custom: ' + str(self.ablation_study.custom_dataset_generator))
+            print('ABLATION: in get_dataset_generator, setting custom, length: ' + len(str(self.ablation_study.custom_dataset_generator)))
+            print('ABLATION: in get_dataset_generator, setting custom, type: ' + type((self.ablation_study.custom_dataset_generator)))
             return self.ablation_study.custom_dataset_generator
         else:
             training_dataset_name = self.ablation_study.hops_training_dataset_name
