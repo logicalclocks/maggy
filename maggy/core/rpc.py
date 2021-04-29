149af3f791f7faf581c7f4855eabd0cee584880b
RiccardoGrigoletto <riccardogrigo@gmail.com>
2021-04-29 16:55:10 +0200

    adrress review comments

--- maggy/core/rpc.py
+++ maggy/core/rpc.py
@@ -657,26 +657,18 @@ class Client(MessageSocket):
             time.sleep(1)
         return trial_id, parameters
 
-    def get_tf_config(self, timeout=60):
-        """Return the """
-        config = None
-        start_time = time.time()
-        while not config and time.time() - start_time < timeout:
-            config = self._request(self.sock, "TF_CONFIG").get("data", None)
-        return config
+    def get_message(self, msg_type, timeout=60):
+        """Return the property of msg_type.
 
-    def get_reservations(self, timeout=60):
-        config = None
-        start_time = time.time()
-        while not config and time.time() - start_time < timeout:
-            config = self._request(self.sock, "RESERVATIONS").get("data", None)
-        return config
+        :param msg_type: The property to request.
+        :param timeout: Waiting time for the request (Default: ''60'')
 
-    def get_exec_config(self, timeout=60):
+        :return the property requested
+        """
         config = None
         start_time = time.time()
         while not config and time.time() - start_time < timeout:
-            config = self._request(self.sock, "EXEC_CONFIG").get("data", None)
+            config = self._request(self.sock, msg_type).get("data", None)
         return config
 
     def stop(self):
